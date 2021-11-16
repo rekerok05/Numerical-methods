eaeb03e06ac21dda33ddeb5a9e9cdd04f2a19b23
rekerok05 <you@example.com>
2021-11-16 21:03:29 +0300

    add folder lab4

---
+++ Lab4/Variables.py
@@ -0,0 +1,8 @@
+import numpy as np
+from numpy import float32
+
+A = np.array(
+    [[2, 1, 0.1], [0.1, 5, 0.72], [-1.2, 3, 1.7]],
+    float32)
+
+f = np.array([[2.1], [-0.7], [-9.86]])
---
+++ Lab4/lab4.py
@@ -0,0 +1,17 @@
+import numpy as np
+import numpy.linalg
+from Lab1 import fuctions
+
+import Variables as var
+
+
+def part1():
+    print("part1")
+
+
+def main():
+    part1()
+
+
+if __name__ == "__main__":
+    main()
