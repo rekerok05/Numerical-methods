bd3bf1cca8dc336bf2551c17320ea17209cd1889
rekerok <rekerok05@gmail.com>
2021-12-14 19:12:58 +0300

    add lab6

--- Lab6/lab6.py
+++ Lab6/lab6.py
@@ -1,75 +1,83 @@
-# coding=utf-8
-from numpy import *
-from numpy.linalg import *
+import numpy as np
 
-inp = open('input2.txt', 'r')
-out = open('output.txt', 'w')
+import Variables as var
 
-A = []
-n = 5
-eps = 10 ** (-15)
 
-for line in inp:  # читаем А
-    temp = [float(x) for x in line.split()]
-    A.append(temp)
+def printValue(name, value):
+    print(f"{name} = \n{value}\n")
 
-a = array(A)
-At = a.transpose()  # находим Аt
-a = dot(At, a)  # перемножаем А на Аt, теперь А - симметрическая
 
-out.write('A:\n {}\n'.format(a))
+def indexofabsmax(matrix):
+    maxL = max(np.abs([matrix[0, 1], matrix[0, 2], matrix[1, 2]]))
+    if maxL == 0.0:
+        return [0, 0, 0]
+    i, j = np.where(np.isclose(abs(matrix), maxL))[0]
+    return [i, j, matrix[i, j]]
 
-# k = (log(eps) - log(sum([abs(el) ** 2 for el in absolute(a - tril(a))])))/log(0.5)
 
-E, U = identity(n), identity(n)
-k = 0
-ak = a  # копия А
+if __name__ == "__main__":
+    tmpA = var.A
+    change = True
+    iteration = 0
+    tmpT = 1
+    print("ИТЕРАЦИИ\n")
+    while np.abs(indexofabsmax(tmpA)[2]) > var.eps:
+        print(f"Шаг {iteration}")
+        iteration += 1
+        i, j = indexofabsmax(tmpA)[0], indexofabsmax(tmpA)[1]
+        print(i, j)
+        fi = 0.5 * np.arctan(2 * tmpA[i, j] / (tmpA[i, i] - tmpA[j, j]))
+        T = np.eye(3)
+        T[i, i] = np.cos(fi)
+        T[i, j] = -np.sin(fi)
+        T[j, i] = np.sin(fi)
+        T[j, j] = np.cos(fi)
+        trT = np.transpose(T)
+        printValue("A", tmpA)
+        printValue("T", T)
+        printValue("trT", trT)
+        if change:
+            tmpT = np.dot(tmpT, T)
+        else:
+            tmpT = T
+            change = False
+        printValue("T*trT", tmpT)
+        tmpVar = np.dot(trT, tmpA)
+        tmpA = np.around(np.dot(tmpVar, T), decimals=7)
+        printValue("tmpA", tmpA)
 
-while (True):
-    L = tril(ak)
-    temp = absolute(ak - L)  # получаем верхний треугольник, без диагонали
-
-    sigma = sum([abs(el) ** 2 for el in temp])  # считаем сумму квадратов недиагональных
-    if (sigma <= eps):  # условие
-        break
-    i, j = unravel_index(temp.argmax(), temp.shape)  # индексы максимального
-
-    alpha = math.atan(2 * ak[i][j] / (ak[i][i] - ak[j][j])) / 2  # считаем угол
-    uk = identity(n)
-    uk[i][i], uk[i][j], uk[j][j], uk[j][i] = cos(alpha), -sin(alpha), cos(alpha), sin(alpha)
-
-    # tan2phi = 2 * ak[i][j] / (ak[i][i] - ak[j][j])
-    # cos2phi = 1 / sqrt(1 + tan2phi ** 2)
-    # cosphi = sqrt((1 + cos2phi) / 2)
-    # sinphi = sign(ak[i][j] / (ak[i][i] - ak[j][j])) * sqrt((1 - cos2phi) / 2)
-    #
-    # uk = identity(n)
-    # uk[i][i], uk[i][j], uk[j][j], uk[j][i] = cosphi, -sinphi, cosphi, sinphi
-
-    ak = dot(dot(uk.transpose(), ak), uk)  # Ukt*A*Uk
-    U = dot(U, uk)  # U*Uk
-    k += 1
-
-out.write('Ak:\n {}\n'.format(ak))
-
-out.write('lambdа:\n {}\n'.format(ak.diagonal()))
-out.write('U:\n {}\n'.format(U))
-out.write('k:\n {}\n'.format(k))
-
-lmax = max(ak.diagonal())  # max lambda
-out.write('lmax:\n {}\n'.format(lmax))
-x = U.transpose()[ak.diagonal().argmax()]  # получаем x
-x /= max(x)  # нормируем
-out.write('x:\n {}\n'.format(x))
-
-r = dot(a, x) - lmax * x  # находим вектор невязки
-out.write('r:\n {}\n'.format(r))
-out.write('||r||:\n {}\n'.format(norm(r, 1)))
-
-p = [4.58801522, -7.82119475, 6.11344651, -2.15665219, 0.2685558]  # из Данилевского
-out.write('p:\n {}\n'.format(p))
-p.insert(0, -1)
-r1 = sum(-(lmax ** (n - i)) * p[i] for i in range(n + 1))  # считаем невязку собственного многочлена Pn(l)
-out.write('Pn(l):\n {}\n'.format(r1))
-
-out.write('eps:\n {}\n'.format(eps))
+    print("-------------------------------------")
+    printValue("lastA", tmpA)
+    printValue("lastT", T)
+    print("-------------------------------------")
+    lambda1 = tmpA[0, 0]
+    lambda2 = tmpA[1, 1]
+    lambda3 = tmpA[2, 2]
+    print("НАХОДИМ СОБСТВЕННЫЕ ЗНАЧЕНИЯ МАТРИЦЫ A\n")
+    printValue("lambda1", lambda1)
+    printValue("lambda2", lambda2)
+    printValue("lambda3", lambda3)
+    print("-------------------------------------")
+    y1 = tmpT[:, 0]
+    y2 = tmpT[:, 1]
+    y3 = tmpT[:, 2]
+    print("НАХОДИМ СОБСТВЕННЫЕ ВЕКТОРЫ МАТРИЦЫ A\n")
+    printValue("y1", y1)
+    printValue("y2", y2)
+    printValue("y3", y3)
+    print("-------------------------------------")
+    print("НОРМИРОВАНИЕ ВЕКТОРОВ В КУБИЧЕСКОЙ НОРМЕ\n")
+    x1 = y1 / np.max(np.abs(tmpT[:, 0]))
+    x2 = y2 / np.max(np.abs(tmpT[:, 1]))
+    x3 = y3 / np.max(np.abs(tmpT[:, 2]))
+    printValue("x1", x1)
+    printValue("x2", x2)
+    printValue("x3", x3)
+    print("-------------------------------------")
+    print("ПРОВЕРКА")
+    print(f"A*x1 = {np.dot(var.A, x1)}")
+    print(f"lambda1*x1 = {np.dot(lambda1, x1)}\n")
+    print(f"A*x2 = {np.dot(var.A, x2)}")
+    print(f"lambda2*x2 = {np.dot(lambda2, x2)}\n")
+    print(f"A*x3 = {np.dot(var.A, x3)}")
+    print(f"lambda3*x3 = {np.dot(lambda3, x3)}\n")
