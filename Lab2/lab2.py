07f46e52ef8d6b14afcf6a6122ad389b9a8b94e6
rekerok <rekerok05@gmail.com>
2022-09-26 15:11:46 +0300

    done lab2

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,38 +1,27 @@
 import numpy as np
-from Lab1 import fuctions, Variables as var
+
+from Lab1 import Variables as Var
+from Lab1 import lab1
 
 
-def part1():
-    inverseMatrix = np.array([])
-    eyeMatrix = np.eye(4)
-    for i in list(range(4)):
-        inverseMatrix = np.append(inverseMatrix,
-                                  np.array(fuctions.makeExcelTable(f"lab2Table{i + 1}", var.A, np.array(eyeMatrix[i]))))
-    inverseMatrix = inverseMatrix.reshape(4, 4)
-    return inverseMatrix
-
-
-
-def part2():
-    print("inverse matrix\n", part1())
-    print("identityMatrix\n", var.A.dot(part1()))
-
-    print("Октаэдрическая норма = ", np.sum(abs(part1())))
-    print("Сферическая норма = ", np.sqrt(np.sum(np.square(abs(part1())))))
-    print("Кубическая норма = ", np.max(abs(part1())))
-
-    print("Октаэдрическая норма исходной = ", 12.7)
-    print("Кубическая норма исходной = ", 0.4782)
-
-    print("Октаэдрическая норма обратной = ", 12.700)
-    print("Кубическая норма обратной = ", 0.4794)
-
-    print("Число обусловленностей (октаэдрическая) = ", 12.7 * 0.4794)
-    print("Число обусловленностей (кубическая) = ", 12.700 * 0.4794)
+def inverse_matrix_method_gauss(matrix: np.ndarray) -> np.ndarray:
+    size_matrix = len(matrix)
+    inverse_matrix = np.array([])
+    for i in np.identity(4, dtype=int):
+        triangleMatrix, solution = lab1.method_Gauss(matrix, i)
+        inverse_matrix = np.append(inverse_matrix, solution)
+    return np.matrix(inverse_matrix).reshape((size_matrix, size_matrix)).T
 
 
 def main():
-    part2()
+    inverse_matrix = inverse_matrix_method_gauss(Var.A)
+    cub_norm_A, oct_norm_A = lab1.norm_matrix(Var.A)
+    print(f"\nОбратная матрица Методом Гаусса:\n{inverse_matrix}\n")
+    print(f"\nОбратная матрица Встроенной функцией:\n{np.linalg.inv(Var.A)}\n")
+    cub_norm_inverse_matrix, oct_norm_inverse_matrix = lab1.norm_matrix(inverse_matrix)
+    print(f"\nПроверка A * A^(-1) = E\n{Var.A.dot(np.linalg.inv(Var.A))}n")
+    print(f"\nЧисло обусловленности (кубическое): {cub_norm_A * cub_norm_inverse_matrix}")
+    print(f"\nЧисло обусловленности (октаэдрическое): {oct_norm_A * oct_norm_inverse_matrix}\n")
 
 
 if __name__ == "__main__":
