be0938d73665921dcf4235377261b88bbbdd5ce9
Milana Antonova <mmf.antonovam@gmail.com>
2022-10-04 13:54:55 +0300

    add first two tasks in lab3

--- Lab3/Variables.py
+++ Lab3/Variables.py
@@ -1,9 +1,10 @@
 import numpy as np
-from numpy import float32
 
 A = np.array(
-    [[3.1, 1, 2.1], [1, 3.6, 2.1], [2.1, 2.1, 4.1]],
-    float32)
+    [[3.3, 1.0, 2.1],
+     [1.0, 3.8, 2.1],
+     [2.1, 2.1, 4.4]],
+    dtype=float)
 
-f = np.array([[2.1], [1.0], [1.1]], dtype=float32)
+f = np.array([[2.1], [1.0], [1.1]], dtype=float)
 
