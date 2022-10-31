fc6a7c88291b1202c9aec00039d7e92032b595dd
Milana Antonova <mmf.antonovam@gmail.com>
2022-10-31 15:26:27 +0300

    add functions to lab5

--- Lab5/Variables.py
+++ Lab5/Variables.py
@@ -1,8 +1,13 @@
 import numpy as np
 from numpy import float32
 
-A = np.array(
-    [[1, 1.5, 2.5, 3.5], [1.5, 1, 2, 1.6], [2.5, 2, 1, 1.7], [3.5, 1.6, 1.7, 1]],
-    float32)
+f = np.array([-2.9, -0.7, -9.86])
 
-f = np.array([[2.1], [1.0], [1.1]], dtype=float32)
+K = 2
+alpha = 0.1 * K
+eps = 0.00005
+
+A = np.array(
+    [[2, 1, alpha],
+     [alpha, 5, 0.72],
+     [-1.2, 3, 1.7]], float32)
--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,120 +1,66 @@
+from Lab4.lab4 import output_matrix, make_matrixB
 import numpy as np
-from numpy import set_printoptions
-from sympy.solvers import solve
-from sympy import Symbol
 import Variables as var
 
 
-def makeFrobenius(matrix):
-    # n = len(matrix)
-    # a = np.array(matrix)
-    # At = a.transpose()  # находим Аt
-    # a = np.dot(At, a)  # перемножаем А на Аt, теперь А - симметрическая
-    # f = a
-    # s = np.identity(n)
-    # for i in range(0, n - 1):
-    #     m = np.identity(n)
-    #     m[n - 2 - i][:] = f[n - 1 - i][:]  # выделяем M^(-1)
-    #     f = np.dot(m, f)  # умножаем A на M^(-1) слева
-    #     f = np.dot(f, np.linalg.inv(m))  # умножаем A на M справа
-    #     s = np.dot(s, np.linalg.inv(m))  # находим S
-    # return f, s
+def iteration_method(B, g, x=None):
+    # newX = np.dot(B, x) + g
+    newX = np.zeros(3)
+    # print(newX)
+    newX[0] = B[0, 0] * x[0] + B[0, 1] * x[1] + B[0, 2] * x[2] + g[0]
+    newX[1] = B[1, 0] * newX[0] + B[1, 1] * x[1] + B[1, 2] * x[2] + g[1]
+    newX[2] = B[2, 0] * newX[0] + B[2, 1] * newX[1] + B[2, 2] * x[2] + g[2]
 
-    # -----------------------------------
-    m3 = np.identity(4)
-    m3[2] = -var.A[3] / var.A[3][2]
-    m3[2][2] /= -var.A[3][2]
-    m3minus = np.identity(4)
-    m3minus[2] = var.A[3]
-    A1 = np.dot(np.dot(m3minus, var.A), m3)
-    printValue("m3", m3)
-    printValue("m3minus", m3minus)
-    printValue("A1", A1)
-    print("\n")
-    # -----------------------------------
-    m2 = np.identity(4)
-    m2[1] = -A1[2] / A1[2][1]
-    m2[1][1] /= -A1[2][1]
-    m2minus = np.identity(4)
-    m2minus[1] = A1[2]
-    A2 = np.dot(np.dot(m2minus, A1), m2)
-    printValue("m2", m3)
-    printValue("m3minus", m3minus)
-    printValue("A2", A2)
-    print("\n")
-    # -----------------------------------
-    m1 = np.identity(4)
-    m1[0] = -A2[1] / A2[1][0]
-    m1[0][0] /= -A2[1][0]
-    m1minus = np.identity(4)
-    m1minus[0] = A2[1]
-    A3 = np.dot(np.dot(m1minus, A2), m1)
-    printValue("m1", m1)
-    printValue("m1minus", m1minus)
-    printValue("A3", A3)
-    print("\n")
-    S = np.dot(np.dot(m3, m2), m1)
-    printValue("S", S)
-    return np.around(A3, decimals=5), S
+    print(f"\nOLD X: x1={x[0]}\tx2={x[1]}\tx3={x[2]}")
+    print(f"NEW X: x1={newX[0]}\tx2={newX[1]}\tx3={newX[2]}")
+    cub_norm_B = np.linalg.norm(B, np.inf)
+    eps = ((1 - cub_norm_B) / cub_norm_B) * var.eps
+    cub_norm = np.linalg.norm(newX - x, np.inf)
+    print(f"norm (x(n+1) - x(n)) = {np.linalg.norm(newX - x, np.inf)}")
+    print(f"eps = {eps}")
+    if cub_norm > eps:
+        return iteration_method(B, g, newX)
+    return newX
 
 
-def printValue(name, value):
-    print(f"{name} = \n{value}")
-
-
-def part1():
-    print("part 1")
-    frobenius, S = makeFrobenius(var.A)
-    print(frobenius)
-    p = frobenius[0, :]
-    print(p)
-    Lambda = np.roots([1, -p[0], -p[1], -p[2], -p[3]])
-    printValue("Lambda", Lambda)
-    print("\n")
-    # ---------------------------
-    y1 = np.array([Lambda[0] ** 3, Lambda[0] ** 2, Lambda[0], 1])
-    y2 = np.array([Lambda[1] ** 3, Lambda[1] ** 2, Lambda[1], 1])
-    y3 = np.array([Lambda[2] ** 3, Lambda[2] ** 2, Lambda[2], 1])
-    y4 = np.array([Lambda[3] ** 3, Lambda[3] ** 2, Lambda[3], 1])
-    printValue("y1", y1)
-    printValue("y2", y2)
-    printValue("y3", y3)
-    printValue("y4", y4)
-    print("\n")
-    # --------------------------
-    x1 = np.dot(S, y1)
-    x2 = np.dot(S, y2)
-    x3 = np.dot(S, y3)
-    x4 = np.dot(S, y4)
-    printValue("x1", x1)
-    printValue("x2", x2)
-    printValue("x3", x3)
-    printValue("x4", x4)
-    print("\n")
-    # ---------------------------
-    print(np.dot(frobenius, y1))
-    print(Lambda[0] * y1)
-    print(np.dot(frobenius, y2))
-    print(Lambda[1] * y2)
-    print(np.dot(frobenius, y3))
-    print(Lambda[2] * y3)
-    print(np.dot(frobenius, y4))
-    print(Lambda[3] * y4)
-    print("\n")
-    # ---------------------------
-    print(np.dot(var.A, x1))
-    print(Lambda[0] * x1)
-    print(np.dot(var.A, x2))
-    print(Lambda[1] * x2)
-    print(np.dot(var.A, x3))
-    print(Lambda[2] * x3)
-    print(np.dot(var.A, x4))
-    print(Lambda[3] * x4)
-    print("\n")
+def norm_matrix_B(A):
+    cub_norm = np.max(list(map(lambda x: np.sum(np.abs(x)), A)))
+    oct_norm = np.max(list(map(lambda x: np.sum(np.abs(x)), A.transpose())))
+    print(
+        f"Кубическая норма матрицы B = {cub_norm}")  # Максимальная среди сумм строк
+    print(
+        f"Октаэдрическая норма матрицы B = {oct_norm}\n")  # Максимальная среди сумм столбцов
+    cub_norm = np.linalg.norm(A, np.inf)
+    oct = np.linalg.norm(A, 1)
+    print(cub_norm)
+    print(oct)
+    return cub_norm, oct_norm
 
 
 def main():
-    part1()
+    output_matrix("A", var.A)
+    output_matrix("f", var.f)
+
+    expandedMatrix = np.insert(arr=var.A, obj=3, values=var.f, axis=1)
+    output_matrix("expandedMatrix (without transformations)", expandedMatrix)
+    expandedMatrix[2] = 2 * expandedMatrix[2] + expandedMatrix[0] - expandedMatrix[1]
+    output_matrix("expandedMatrix (with transformations", expandedMatrix)
+
+    D = np.zeros((3, 3))
+    np.fill_diagonal(D, expandedMatrix.diagonal())
+    output_matrix("D", D)
+    C = np.linalg.inv(D)
+    output_matrix("C", C)
+    B = make_matrixB(expandedMatrix[:, :-1], C)
+    output_matrix("B", B)
+    norm_matrix_B(B)
+    g = np.dot(C, expandedMatrix[:, -1])
+    output_matrix("g", g)
+
+    x = iteration_method(B, g, g)
+    # vector = np.dot(var.A, x) - var.f
+    # print(f"\nвектора невязки = {vector}")
+    # print(f"кубическая норма вектора невязки = {np.max(np.abs(vector))}")
 
 
 if __name__ == "__main__":
