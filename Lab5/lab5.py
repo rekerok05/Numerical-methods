85e90b9f89f1ef60f5c00f90afc2e3708d67164b
Sergey <rekerok05@gmail.com>
2022-11-04 15:35:28 +0300

    done lab5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,9 +1,10 @@
-from Lab4.lab4 import output_matrix, make_matrixB
 import numpy as np
+
 import Variables as var
+from Lab4.lab4 import output_matrix, make_matrixB
 
 
-def iteration_method(B, g, x=None):
+def iteration_method(B, g, x=None, eps = None):
     # newX = np.dot(B, x) + g
     newX = np.zeros(3)
     # print(newX)
@@ -13,27 +14,21 @@ def iteration_method(B, g, x=None):
 
     print(f"\nOLD X: x1={x[0]}\tx2={x[1]}\tx3={x[2]}")
     print(f"NEW X: x1={newX[0]}\tx2={newX[1]}\tx3={newX[2]}")
-    cub_norm_B = np.linalg.norm(B, np.inf)
-    eps = ((1 - cub_norm_B) / cub_norm_B) * var.eps
-    cub_norm = np.linalg.norm(newX - x, np.inf)
-    print(f"norm (x(n+1) - x(n)) = {np.linalg.norm(newX - x, np.inf)}")
-    print(f"eps = {eps}")
-    if cub_norm > eps:
-        return iteration_method(B, g, newX)
+    cub_norm_x = np.linalg.norm(newX - x, np.inf)
+    print(f"norm (x(n+1) - x(n)) = {cub_norm_x}")
+    print(f"eps1 = {eps}")
+    if cub_norm_x > eps:
+        return iteration_method(B, g, newX, eps)
     return newX
 
 
 def norm_matrix_B(A):
-    cub_norm = np.max(list(map(lambda x: np.sum(np.abs(x)), A)))
-    oct_norm = np.max(list(map(lambda x: np.sum(np.abs(x)), A.transpose())))
+    cub_norm = np.linalg.norm(A, np.inf)
+    oct_norm = np.linalg.norm(A, 1)
     print(
         f"Кубическая норма матрицы B = {cub_norm}")  # Максимальная среди сумм строк
     print(
         f"Октаэдрическая норма матрицы B = {oct_norm}\n")  # Максимальная среди сумм столбцов
-    cub_norm = np.linalg.norm(A, np.inf)
-    oct = np.linalg.norm(A, 1)
-    print(cub_norm)
-    print(oct)
     return cub_norm, oct_norm
 
 
@@ -57,10 +52,23 @@ def main():
     g = np.dot(C, expandedMatrix[:, -1])
     output_matrix("g", g)
 
-    x = iteration_method(B, g, g)
-    # vector = np.dot(var.A, x) - var.f
-    # print(f"\nвектора невязки = {vector}")
-    # print(f"кубическая норма вектора невязки = {np.max(np.abs(vector))}")
+    H = np.array([[0, 0, 0], [B[1, 0], 0, 0], [B[2, 0], B[2, 1], 0]])
+    output_matrix("H", H)
+    F = B - H
+
+    EHF = np.dot(np.linalg.inv(np.identity(3) - H), F)
+    cube_norm_EHF = np.linalg.norm(EHF, np.inf)
+    output_matrix("(E-H)^-1 * F", EHF)
+    print(f"norm EHF = {cube_norm_EHF}")
+    eps1 = ((1 - cube_norm_EHF)/(cube_norm_EHF)) * var.eps
+    print(f"eps1 = {eps1}")
+    output_matrix("F", F)
+    x = iteration_method(B, g, g, eps1)
+
+    vector = np.dot(var.A, x) - var.f
+    print(f"\nвектора невязки = {vector}")
+    print(f"кубическая норма вектора невязки = {np.max(np.abs(vector))}")
+    print(f"eps = {var.eps}")
 
 
 if __name__ == "__main__":
